4b49bd6acbb456d28c47e54654924aa399076d0a
Shashwat Anand <shashwatanand1801@gmail.com>
2022-09-04 18:06:12 +0530

    Intialized the project and added header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style/Header.css'
+
+const App = () => (
+  <div className='heading'>
+    <h1> Personal Finance Tracker</h1>
+  </div>
+);
+
+export default App;
